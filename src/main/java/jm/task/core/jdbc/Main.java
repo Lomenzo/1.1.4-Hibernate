73ccf77466470351f0a22fd71f1183c74c3ef507
Lomenzo <LomenzoJVM@gmail.com>
2022-08-30 16:10:37 +0800

    v.4 first Hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,9 +1,46 @@
 package jm.task.core.jdbc;
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
+        //UserServiceImpl userService = new UserServiceImpl();
+        //userService.createUsersTable();
+
+        Configuration configuration = new Configuration();
+        Properties settings = new Properties();
+        settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        settings.put(Environment.URL, "jdbc:mysql://localhost/dedusers");
+        settings.put(Environment.USER, "root");
+        settings.put(Environment.PASS, "1Dothisf");
+        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL52Dialect");
+        settings.put(Environment.SHOW_SQL, "true");
+        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+        configuration.setProperties(settings);
+        configuration.addAnnotatedClass(User.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+
+        Session session = sessionFactory.getCurrentSession();
+
+        session.beginTransaction();
+        User user = new User("Petr", "Petrov", (byte)45);
+        System.out.println(user);
+        session.save(user);
+        session.beginTransaction().commit();
+
+        sessionFactory.close();
+
+
+
+        /*UserServiceImpl userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Petr", "Petrov", (byte)45);
         userService.saveUser("Liza", "Smirnova", (byte)33);
@@ -12,7 +49,7 @@ public class Main {
         userService.getAllUsers();
         userService.removeUserById(1);
         userService.cleanUsersTable();
-        userService.dropUsersTable();
+        userService.dropUsersTable();*/
     }
 }
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,53 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import javax.imageio.spi.ServiceRegistry;
+
 import java.sql.*;
+import java.util.Properties;
 
 
 public class Util {
     public static Connection connectMethod() throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/dedusers", "root", "1Dothisf");
         return conn;
+    }
+
+    private static SessionFactory sessionFactory = getSessionFactory();
+
+    private static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost/dedusers");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "1Dothisf");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+
+
+            } catch (Exception e) {
+                System.out.println("Исключение при создании sessionFactory");
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
+    public static SessionFactory getSession() {
+        return sessionFactory;
     }
 }
